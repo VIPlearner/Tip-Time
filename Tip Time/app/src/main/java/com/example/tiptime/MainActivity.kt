4789276cc8660768158ab34f27328363db3a9bf4
Joshua Owolabi <joshuaoluwasegun99@gmail.com>
2022-10-01 18:21:42 +0100

    Added functionality to the app Added a failsafe if user does not input a value

--- Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculate.setOnClickListener{calculateTip()}
+    }
+    private fun calculateTip(){
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if (cost == null){
+            binding.tipAmount.text = getString(R.string.invalid_amount)
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost!!
+        if (binding.tipRounding.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+
+        val totalCost = cost + tip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
+
+
     }
 }
